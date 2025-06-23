368d9d0c618c1458f29df51e9d5e94352f2f80a4
dong <dong58055@gmail.com>
2025-06-24 01:43:04 +0900

    更新

--- src/main/java/com/souko/controllar/HelloController.java
+++ src/main/java/com/souko/controllar/HelloController.java
@@ -18,23 +18,26 @@ import com.souko.service.UserService;
 @RestController
 @RequestMapping()
 public class HelloController {
-	
+
 	@Autowired
 	private UserService userService;
+	
+	private int i =5;
+	
 	@GetMapping("/test")
 	public List<User> list() {
-		//return userService.list();
+		// return userService.list();
 		return userService.selectAll();
 	}
-	
+
 	@PostMapping("/listPage")
-	public List<User> listPage(@RequestBody LoginForm loginForm){
-		Page<User> iPage = new Page<>(loginForm.getPageNum(),loginForm.getPageSize());
+	public List<User> listPage(@RequestBody LoginForm loginForm) {
+		Page<User> iPage = new Page<>(loginForm.getPageNum(), loginForm.getPageSize());
 		LambdaQueryWrapper<User> lqw = new LambdaQueryWrapper<>();
-		//lqw.like(User::getName, loginForm.getName());
+		// lqw.like(User::getName, loginForm.getName());
 		lqw.like(User::getName, loginForm.getParam().get("name"));
 		Page<User> page2 = userService.page(iPage, lqw);
 		return page2.getRecords();
-		
+
 	}
 }
