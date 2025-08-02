19aaf05bc7c014a215ceed21dad56ec1413a7e64
dong <dong58055@gmail.com>
2025-08-03 01:29:33 +0900

    登录完成

--- src/main/java/com/souko/controllar/HelloController.java
+++ src/main/java/com/souko/controllar/HelloController.java
@@ -3,7 +3,9 @@ package com.souko.controllar;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.StringUtils;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.souko.SoukoApplication;
 import com.souko.dto.LoginForm;
 import com.souko.dto.Result;
 import com.souko.entity.User;
@@ -22,8 +25,14 @@ import com.souko.service.UserService;
 @RequestMapping("/user")
 public class HelloController {
 
+    private final SoukoApplication soukoApplication;
+
     @Autowired
     private UserService userService;
+
+    HelloController(SoukoApplication soukoApplication) {
+        this.soukoApplication = soukoApplication;
+    }
 
     // 测试
     @GetMapping("/test")
@@ -31,36 +40,56 @@ public class HelloController {
         List<User> users = userService.list();
         return Result.success(users, users.size());
     }
-
+    //新增用户
     @PostMapping("/add")
     public Result save(@RequestBody User user) {
         //return Result.success(userService.save(user));
         return userService.save(user) ? Result.success("保存成功") : Result.fail();
     }
-
-    @PostMapping("/listPage")
-    public Result listPage(@RequestBody LoginForm loginForm) {
-        Page<User> iPage = new Page<>(loginForm.getPageNum(), loginForm.getPageSize());
-        LambdaQueryWrapper<User> lqw = new LambdaQueryWrapper<>();
-        String name = (String) loginForm.getParam().get("name");
-        String sex = (String) loginForm.getParam().get("sex");
-        lqw.like(StringUtils.isNotBlank(name), User::getName, loginForm.getParam().get("name"));
-        lqw.eq(StringUtils.isNotBlank(sex), User::getSex, loginForm.getParam().get("sex"));
-        Page<User> page2 = userService.page(iPage, lqw);
-        return Result.success(page2.getRecords(), (int) page2.getTotal());
+    //编辑用户
+    @PostMapping("/update")
+    public Result update(@RequestBody User user) {
+    	return userService.updateById(user)? Result.success("更新成功"): Result.fail("更新失败");
     }
+    
+	@PostMapping("/listPage")
+	public Result listPage(@RequestBody LoginForm loginForm) {
+		Page<User> iPage = new Page<>(loginForm.getPageNum(), loginForm.getPageSize());
+		LambdaQueryWrapper<User> lqw = new LambdaQueryWrapper<>();
+		String name = (String) loginForm.getParam().get("name");
+		String sex = (String) loginForm.getParam().get("sex");
+		lqw.like(StringUtils.isNotBlank(name), User::getName, loginForm.getParam().get("name"));
+		lqw.eq(StringUtils.isNotBlank(sex), User::getSex, loginForm.getParam().get("sex"));
+		Page<User> page2 = userService.page(iPage, lqw);
+		return Result.success(page2.getRecords(), (int) page2.getTotal());
+	}
 
     //id
+    //@GetMapping("/findByNo")
     @GetMapping("/findByNo")
     public Result findNo(@RequestParam String no) {
 //        LambdaQueryWrapper<User> lqw = new LambdaQueryWrapper<>();
 //        lqw.eq(User::getNo, no);
 //        User user = userService.getOne(lqw);
         User user = userService.lambdaQuery().eq(User::getNo,no).one();
+        
         if(user==null){
             return Result.fail("用户不存在");
         }
         return Result.success(user);
-
     }
+    //删除
+    @DeleteMapping("/delete/{id}")
+    public Result delete(@PathVariable int id) {
+    	return userService.removeById(id)? Result.success("删除成功") : Result.fail("删除失败");
+    	
+    }
+    //登录
+    @PostMapping("/login")
+    public Result login(@RequestBody User user) {
+    	User one = userService.lambdaQuery().eq(User::getNo, user.getNo())
+    	.eq(User::getPassword, user.getPassword()).one();
+    	return one==null?Result.fail():Result.success(one);
+    }
+    
 }
--- src/main/java/com/souko/entity/User.java
+++ src/main/java/com/souko/entity/User.java
@@ -1,5 +1,7 @@
 package com.souko.entity;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.Data;
 
 @Data
@@ -7,8 +9,13 @@ public class User {
 	    private Integer id;
 	    private String no;
 	    private String name;
+	    //序列化：从后端对象转换成 JSON，password 字段被忽略，不会返回给前端。
+	    //反序列化：前端发送 JSON 请求体时，password 字段也会被忽略，导致后端接收的 password 是 null。
+	    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+	    //传给前端时列化为 JSON 时自动忽略敏感字段
 	    private String password;
 	    private Integer age;
+		//传给前端时要修改成字符串
 	    private Integer sex;
 	    private String phone;
 	    private Integer roleId;
